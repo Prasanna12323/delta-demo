ee12f2116aeccd8739a857eb285f9632ec3b86d9
Prasanna12323 <167713361+Prasanna12323@users.noreply.github.com>
2025-03-20 22:52:38 +0530

    Add files via upload

---
+++ 5_float.cpp
@@ -0,0 +1,10 @@
+// #include<iostream> 
+// using namespace std;
+// int main()
+// {
+//     float x,y ;
+//     x =3 , y =9;
+//     cout << x << endl << y << endl << x+y ;
+//     cout << endl <<x/y;
+//     return 0;
+// }
